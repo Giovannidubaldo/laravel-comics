e42a412fe7af3ce3d4752253a17d45e48521201b
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-12 15:52:48 +0100

    Added ul on header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,27 @@
 <header>
     <div class="container">
-        <div class="row">
+        <div class="row d-flex align-items-center">
             <div class="col-2">
                 <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="" class="img-fluid">
             </div>
             <div class="col-8">
-                <ul class="list-unstyled"></ul>
+                <ul class="list-unstyled d-flex justify-content-center">
+                    <li>
+                        <a href="{{ route('homepage') }}"
+                            class="nav-link {{ Route::currentRouteName() === 'hompeage' ? 'text-primary' : '' }}">home
+                        </a>
+                    </li>
+                    <li><a href="#">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">shop</a></li>
+                </ul>
             </div>
             <div class="col-2"></div>
         </div>
