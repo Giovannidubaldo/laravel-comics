255fe4bdef8ad1c7d67792a7113c6998c9ddb259
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-12 15:32:17 +0100

    Added logo on header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="" class="img-fluid">
+            </div>
+            <div class="col-8">
+                <ul class="list-unstyled"></ul>
+            </div>
+            <div class="col-2"></div>
+        </div>
+    </div>
+</header>
